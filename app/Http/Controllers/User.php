0e5f05a9383cf9ca06b09a3b9d601e0c5b06b228
Intan Nora <intan.norasyahira@outlook.com>
2020-12-10 11:41:45 +0800

    latest changes to code

--- app/Http/Controllers/User.php
+++ app/Http/Controllers/User.php
@@ -11,7 +11,8 @@ class User extends Controller
 	public function index(){
 
 		$users = Http::get("https://gorest.co.in/public-api/users");
-		return view('index', ['users' => $users['data']]);
+		//return view('index', ['users' => $users['data']]);
+		print_r($users->header('Content-Type', 'text/plain'));
 
 	}
 
